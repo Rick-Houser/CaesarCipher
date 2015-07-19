d85506b1428b8290b3c43f04ef98ff10d0b3205b
FrackenToaster <frakentoaster@gmail.com>
2015-07-19 15:39:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Caesar's Cipher
+
+A small and effective Cipher that will encrypt and decrypt strings.
+Provide a string or array and the number to shift the result by.
---
+++ cipher.rb
@@ -0,0 +1,18 @@
+def cipher(array, shift)
+	temp = []
+	bytes = array.unpack('c*') 					 # Convert chars to binary
+	bytes.each { |i| temp << i + shift } # Increment binary by shift
+	result = temp.pack('c*')						 # Convert binary back to chars
+	puts result
+end
+
+def decipher(array, shift)
+	temp = []
+	bytes = array.unpack('c*')
+	bytes.each { |i| temp << i - shift }
+	result = temp.pack('c*')
+	puts result	
+end
+
+cipher('What a string!', 5)
+decipher('\mfy%f%xywnsl&', 5)
